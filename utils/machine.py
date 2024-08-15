d3608fbbf6536623748e9e7eabf354a10e336565
Kim <kimhaafi@gmail.com>
2024-08-15 22:41:50 +0200

    wip

--- utils/machine.py
+++ utils/machine.py
@@ -40,26 +40,30 @@ def stream_gcode(ser, gcode_path, max_commands):
 
     with open(gcode_path, "r") as file:
         send_wake_up(ser)
-        count_ok = 0
+        commands = 0
+
         for line in file:
             cleaned_line = remove_eol_chars(remove_comment(line))
             if cleaned_line:  # checks if string is empty
-                if count_ok % max_commands == 0:
-                    Event().wait(0.1)
                 print("Sending gcode:" + str(cleaned_line))
-                wait_for_buffer(ser)
-                command = str.encode(line + "\n")
-                ser.write(command)  # Send g-code
 
-                grbl_out = ser.readline()
-
-                grbl_response = grbl_out.strip().decode("utf-8")
                 if line.startswith("G") or "$H" in line:
                     while grbl_response != "ok":
                         ser.write(command)  # Send g-code
                         grbl_out = ser.readline()
                         grbl_response = grbl_out.strip().decode("utf-8")
 
-                    count_ok += 1
+                else:
+                    command = str.encode(line + "\n")
+                    ser.write(command)  # Send g-code
+
+                    grbl_out = ser.readline()
+
+                    grbl_response = grbl_out.strip().decode("utf-8")
+
+                commands += 1
+                while commands > max_commands:
+                    wait_for_buffer(ser)
+                    commands = 0
 
         print("End of gcode")
